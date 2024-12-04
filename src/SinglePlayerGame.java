1651a9da33cc33355756c694ff02f7249c2aff6d
Dhanalaxmi-Musku <135241955+Dhanalaxmi-Musku@users.noreply.github.com>
2024-12-04 14:16:34 +0530

    Added:starting position

--- src/SinglePlayerGame.java
+++ src/SinglePlayerGame.java
@@ -1,3 +1,15 @@
+import java.util.Random;
+
 public class SinglePlayerGame {
+	private static final int WINNING_POSITION = 100;
+    private static final int START_POSITION = 0;
+    private int playerPosition;
+    private int diceRollCount;
+    private Random random;
+    public SinglePlayerGame() {
+        this.playerPosition = START_POSITION;
+        this.diceRollCount = 0;
+        this.random = new Random();
+    }
 
 }
